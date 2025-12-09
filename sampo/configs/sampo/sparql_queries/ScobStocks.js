dae6b8dce96682af40ab94e9cd94b844f7d6c098
ceetto <jahid.chetti@ugent.be>
2025-12-09 09:14:12 +0100

    feat: securities extra chart

--- sampo/configs/sampo/sparql_queries/ScobStocks.js
+++ sampo/configs/sampo/sparql_queries/ScobStocks.js
@@ -1,73 +1,11 @@
-export const stockPropertiesInstancePage = `
-{
-    ?id bhf:hasSharetype ?sharetype__id .
-    bind(?sharetype__id as ?sharetype__prefLabel)
-    bind(?id as ?uri__id)
-    bind(?id as ?uri__prefLabel)
-}
-UNION
-{
-    ?stockcorp__id bhf:hasStock ?id .
-    ?corporation__id bhf:hasStockCorporation ?stockcorp__id .
-    optional {?stockcorp__id bhf:startDate ?corporation__startDate .}
-    optional {?stockcorp__id bhf:endDate ?corporation__endDate .}
-    bind(concat(
-      str(?corporation__id),
-      " [",
-      COALESCE(str(?corporation__startDate), "????"), 
-      " - ",
-      COALESCE(str(?corporation__endDate), "????"),
-      "]"
-    ) as ?corporation__prefLabel)
-    BIND(CONCAT("/corporations/page/", STRAFTER(STR(?corporation__id), "corporation/")) AS ?corporation__dataProviderUrl)
-}
-UNION
-{
-    ?id bhf:hasName ?name__id .
-    ?name__id rdfs:label ?name .
-    optional {?name__id bhf:startDate ?name__startDate .}
-    optional {?name__id bhf:endDate ?name__endDate .}
-    bind(concat(
-      str(?name),
-      " [",
-      COALESCE(str(?name__startDate), "????"), 
-      " - ",
-      COALESCE(str(?name__endDate), "????"),
-      "]"
-    ) as ?name__prefLabel)
-}
-union
-{
-    ?id bhf:hasStockQuantity ?stockQuantity__id .
-    ?stockQuantity__id bhf:hasCurrency ?currency__id .
-    ?currency__id foaf:name ?currency__prefLabel .
-}
-union
-{
-    ?id bhf:hasStockExchange ?stockExchange__id .
-    ?stockExchange__id bhf:hasName ?stockExchangeName__id .
-    ?stockExchangeName__id rdfs:label ?stockExchangeName__prefLabel .
-}
-union 
-{
-    ?id bhf:hasStockType ?stocktype__id .
-    ?stocktype__id bhf:typeName ?stocktype__prefLabel .
-}
-union
-{
-    ?id bhf:hasNotation ?notation__id .
-    ?notation__id bhf:hasSector ?sector__id .
-    ?sector__id bhf:hasName ?sectorName__id .
-    ?sectorName__id rdfs:label ?sectorName__prefLabel
-}
-`
-
 export const stocksProperties = `
 {
-    bind(?id as ?uri__prefLabel)
     ?id bhf:scobID ?scobID__id .
     bind(?scobID__id as ?scobID__prefLabel)
+    bind(?id as ?uri__id)
+    bind(?id as ?uri__prefLabel)
     BIND(CONCAT("/securities/page/", str(?scobID__id)) AS ?scobID__dataProviderUrl)
+    BIND(?scobID__dataProviderUrl AS ?name__dataProviderUrl)
 }
 union
 {
@@ -143,17 +81,18 @@ where {
 ORDER BY ?date
 `
 
-export const stocksGraphOpen = `
-SELECT DISTINCT ?category ?count ?categoryLabel
+export const stocksGraphMinMax = `
+SELECT DISTINCT ?date ?min ?max
 where {
     bind(<ID> as ?id)
     ?id bhf:hasNotation/bhf:hasNotationPrice ?price__id .
-    ?price__id bhf:priceDay ?category .
-    bind(?category as ?categoryLabel)
-    ?price__id bhf:openValue ?count .
+    ?price__id bhf:priceDay ?date .
+    optional {?price__id bhf:maxValue ?max .}
+    optional {?price__id bhf:minValue ?min .}
 }
-ORDER BY ?category
+ORDER BY ?date
 `
+
 
 export const facetResultSetQueryBelhisfirm = `
   SELECT *
